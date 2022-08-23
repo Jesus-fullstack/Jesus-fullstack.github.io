bed3c8cead108674c3a3bafb00e963b295dd81aa
Jesus Araujo <Jesusycory234@gmail.com>
2022-08-23 09:54:07 -0400

    Inicia la practica de Javascript en github

---
+++ logic.js
@@ -0,0 +1,32 @@
+let x = document.querySelector("h1");
+x.innerText = "gracias a <br> dios";
+let azul = document.querySelector(".azul");
+azul.innerHTML ="por favor ponte azul";
+let amarillo = document.querySelector("#amarillo");
+let input1 = document.querySelector(".primero");
+let input2 = document.querySelector(".segundo");
+let titulo = document.createElement("p")
+let mensaje = document.querySelector("#resultado");
+
+
+amarillo.innerHTML ="por favor ponte amarillo";
+amarillo.classList.add("amarillo")
+azul.classList.add("rojo")
+amarillo.classList.remove("amarillo")
+
+
+let invisible = document.createElement("img")
+titulo.innerHTML ="Calculador geckoniana (Solo suma)";
+invisible.setAttribute("src","images\\favicon_gecko_-removebg-preview.png")
+invisible.setAttribute("height","100px")
+amarillo.innerHTML = "";
+amarillo.append(invisible)
+amarillo.append(titulo)
+
+function suma() {
+let z = Number(input1.value) + Number(input2.value);
+const resultado = "Este es el tan esperado resultado " + z;
+mensaje.innerHTML = resultado;
+}
+
+
